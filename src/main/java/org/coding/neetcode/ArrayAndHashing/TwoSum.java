d1e94dda2001c4e6a5b2c3dd8186b707fbcefd5e
Music App <gmeghagupta@gmail.com>
2024-07-13 19:33:44 +0530

    Two Sum: https://neetcode.io/problems/two-integer-sum

---
+++ src/main/java/org/coding/neetcode/ArrayAndHashing/TwoSum.java
@@ -0,0 +1,25 @@
+package org.coding.neetcode.ArrayAndHashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        // https://neetcode.io/problems/two-integer-sum
+        // Appraoch keep moving to array and store values in map with its value and index
+        // , and keep searching for difference between target and current values if found return
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int difference = target - num;
+            if (map.containsKey(difference)) {
+                return new int[]{map.get(difference), i};
+            }
+            map.put(num, i);
+        }
+        return null;
+    }
+}
